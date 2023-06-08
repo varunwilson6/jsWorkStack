45e665e68a46be4bf6c322b4285218e5203861ac
varunwilson6 <47499003+varunwilson6@users.noreply.github.com>
2023-06-08 10:08:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jsWorkStack
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zur6zt)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+function writeToHtml() {
+  // Write Javascript code!
+  const appDiv = document.getElementById('app');
+  appDiv.innerHTML = `<h1>Code in Log</h1>`;
+}
+
+writeToHtml();
+
+
+
